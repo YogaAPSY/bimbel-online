2d2bda4e8b4982328fc8dcd8728710a7b8d1fc91
Muhammad Frandika <muhammadfrade@gmail.com>
2020-06-06 21:08:38 +0700

    penambahan link terkait

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
 	<div class="container d-flex align-items-center">
-		<a class="navbar-brand" href="index.html">B'Smart</a>
+		<a class="navbar-brand" href="#">B'Smart</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="oi oi-menu"></span> Menu
 		</button>
