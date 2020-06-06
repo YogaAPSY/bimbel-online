9a2bc5199521ccceec620c8f6048a8549da1ccdf
Muhammad Frandika <muhammadfrade@gmail.com>
2020-06-06 09:57:31 +0700

    nambah navbar dan footer di view

---
+++ application/views/footer.php
@@ -0,0 +1,119 @@
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-md-6 col-lg-3">
+                <div class="ftco-footer-widget mb-5">
+                    <h2 class="ftco-heading-2">Have a Questions?</h2>
+                    <div class="block-23 mb-3">
+                        <ul>
+                            <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain
+                                    View, San Francisco, California, USA</span></li>
+                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929
+                                        210</span></a></li>
+                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="ftco-footer-widget mb-5">
+                    <h2 class="ftco-heading-2">Recent Blog</h2>
+                    <div class="block-21 mb-4 d-flex">
+                        <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
+                        <div class="text">
+                            <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a>
+                            </h3>
+                            <div class="meta">
+                                <div><a href="#"><span class="icon-calendar"></span> Dec 25, 2018</a></div>
+                                <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                                <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="block-21 mb-5 d-flex">
+                        <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
+                        <div class="text">
+                            <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a>
+                            </h3>
+                            <div class="meta">
+                                <div><a href="#"><span class="icon-calendar"></span> Dec 25, 2018</a></div>
+                                <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                                <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="ftco-footer-widget mb-5 ml-md-4">
+                    <h2 class="ftco-heading-2">Links</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>About</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Services</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Deparments</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="ftco-footer-widget mb-5">
+                    <h2 class="ftco-heading-2">Subscribe Us!</h2>
+                    <form action="#" class="subscribe-form">
+                        <div class="form-group">
+                            <input type="text" class="form-control mb-2 text-center" placeholder="Enter email address">
+                            <input type="submit" value="Subscribe" class="form-control submit px-3">
+                        </div>
+                    </form>
+                </div>
+                <div class="ftco-footer-widget mb-5">
+                    <h2 class="ftco-heading-2 mb-0">Connect With Us</h2>
+                    <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+
+                <p>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script>
+                        document.write(new Date().getFullYear());
+                    </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- loader -->
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+        <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+        <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg></div>
+
+
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery-migrate-3.0.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/jquery.animateNumber.min.js"></script>
+<script src="js/scrollax.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
+</script>
+<script src="js/google-map.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+
+</html>
--- application/views/landing.php
+++ application/views/landing.php
@@ -1,60 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<title>Kiddos - Free Bootstrap 4 Template by Colorlib</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great" rel="stylesheet">
-
-	<link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-	<link rel="stylesheet" href="css/animate.css">
-
-	<link rel="stylesheet" href="css/owl.carousel.min.css">
-	<link rel="stylesheet" href="css/owl.theme.default.min.css">
-	<link rel="stylesheet" href="css/magnific-popup.css">
-
-	<link rel="stylesheet" href="css/aos.css">
-
-	<link rel="stylesheet" href="css/ionicons.min.css">
-
-	<link rel="stylesheet" href="css/flaticon.css">
-	<link rel="stylesheet" href="css/icomoon.css">
-	<link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
-		<div class="container d-flex align-items-center">
-			<a class="navbar-brand" href="index.html">Bimble Online</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
-				aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="oi oi-menu"></span> Menu
-			</button>
-			<div class="collapse navbar-collapse" id="ftco-nav">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active"><a href="index.html" class="nav-link pl-0">Home</a></li>
-					<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-					<li class="nav-item"><a href="teacher.html" class="nav-link">Teacher</a></li>
-					<li class="nav-item"><a href="courses.html" class="nav-link">Courses</a></li>
-					<li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li>
-					<li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-					<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-	<!-- END nav -->
-
 	<section class="home-slider owl-carousel">
 		<div class="slider-item" style="background-image:url(images/bg_1.jpg);">
 			<div class="overlay"></div>
 			<div class="container">
-				<div class="row no-gutters slider-text align-items-center justify-content-center"
-					data-scrollax-parent="true">
+				<div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
 					<div class="col-md-8 text-center ftco-animate">
 						<h1 class="mb-4">Anak-anak adalah yang terbaik <span> penjelajah di dunia</span></h1>
 						<p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Daftar Sekarang</a></p>
@@ -66,8 +14,7 @@
 		<div class="slider-item" style="background-image:url(images/bg_2.jpg);">
 			<div class="overlay"></div>
 			<div class="container">
-				<div class="row no-gutters slider-text align-items-center justify-content-center"
-					data-scrollax-parent="true">
+				<div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
 					<div class="col-md-8 text-center ftco-animate">
 						<h1 class="mb-4">Belajar Semupurna <span> Untuk Anak Anda</span></h1>
 						<p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Daftar Sekarang</a></p>
@@ -155,8 +102,7 @@
 					<div class="row mt-5">
 						<div class="col-lg-6">
 							<div class="services-2 d-flex">
-								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
-										class="flaticon-security"></span></div>
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-security"></span></div>
 								<div class="text">
 									<h3>Keselamatan</h3>
 									<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
@@ -165,8 +111,7 @@
 						</div>
 						<div class="col-lg-6">
 							<div class="services-2 d-flex">
-								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
-										class="flaticon-reading"></span></div>
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-reading"></span></div>
 								<div class="text">
 									<h3>Kelas Reguler</h3>
 									<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
@@ -175,8 +120,7 @@
 						</div>
 						<div class="col-lg-6">
 							<div class="services-2 d-flex">
-								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
-										class="flaticon-diploma"></span></div>
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-diploma"></span></div>
 								<div class="text">
 									<h3>Guru Bersertifikat</h3>
 									<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
@@ -185,8 +129,7 @@
 						</div>
 						<div class="col-lg-6">
 							<div class="services-2 d-flex">
-								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
-										class="flaticon-education"></span></div>
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-education"></span></div>
 								<div class="text">
 									<h3>Ruang Kelas yang Cukup</h3>
 									<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
@@ -195,8 +138,7 @@
 						</div>
 						<div class="col-lg-6">
 							<div class="services-2 d-flex">
-								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
-										class="flaticon-jigsaw"></span></div>
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-jigsaw"></span></div>
 								<div class="text">
 									<h3>Pelajaran Kreatif</h3>
 									<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
@@ -205,8 +147,7 @@
 						</div>
 						<div class="col-lg-6">
 							<div class="services-2 d-flex">
-								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
-										class="flaticon-kids"></span></div>
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-kids"></span></div>
 								<div class="text">
 									<h3>Fasilitas olahraga</h3>
 									<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
@@ -385,8 +326,7 @@
 		</div>
 	</section>
 
-	<section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(images/bg_4.jpg);"
-		data-stellar-background-ratio="0.5">
+	<section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(images/bg_4.jpg);" data-stellar-background-ratio="0.5">
 		<div class="container">
 			<div class="row justify-content-center mb-5 pb-2">
 				<div class="col-md-8 text-center heading-section heading-section-black ftco-animate">
@@ -533,8 +473,7 @@
 		</div>
 	</section>
 
-	<section class="ftco-section ftco-consult ftco-no-pt ftco-no-pb" style="background-image: url(images/bg_5.jpg);"
-		data-stellar-background-ratio="0.5">
+	<section class="ftco-section ftco-consult ftco-no-pt ftco-no-pb" style="background-image: url(images/bg_5.jpg);" data-stellar-background-ratio="0.5">
 		<div class="container">
 			<div class="row justify-content-end">
 				<div class="col-md-6 py-5 px-md-5 bg-primary">
@@ -574,8 +513,7 @@
 						</div>
 						<div class="d-md-flex">
 							<div class="form-group">
-								<textarea name="" id="" cols="30" rows="2" class="form-control"
-									placeholder="Message"></textarea>
+								<textarea name="" id="" cols="30" rows="2" class="form-control" placeholder="Message"></textarea>
 							</div>
 							<div class="form-group ml-md-4">
 								<input type="submit" value="Request A Quote" class="btn btn-secondary py-3 px-4">
@@ -673,8 +611,7 @@
 			<div class="row">
 				<div class="col-md-6 col-lg-4 ftco-animate">
 					<div class="blog-entry">
-						<a href="blog-single.html" class="block-20 d-flex align-items-end"
-							style="background-image: url('images/image_1.jpg');">
+						<a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_1.jpg');">
 							<div class="meta-date text-center p-2">
 								<span class="day">27</span>
 								<span class="mos">January</span>
@@ -686,8 +623,7 @@
 							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
 								there live the blind texts.</p>
 							<div class="d-flex align-items-center mt-4">
-								<p class="mb-0"><a href="#" class="btn btn-secondary">Read More <span
-											class="ion-ios-arrow-round-forward"></span></a></p>
+								<p class="mb-0"><a href="#" class="btn btn-secondary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
 								<p class="ml-auto mb-0">
 									<a href="#" class="mr-2">Admin</a>
 									<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
@@ -698,8 +634,7 @@
 				</div>
 				<div class="col-md-6 col-lg-4 ftco-animate">
 					<div class="blog-entry">
-						<a href="blog-single.html" class="block-20 d-flex align-items-end"
-							style="background-image: url('images/image_2.jpg');">
+						<a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_2.jpg');">
 							<div class="meta-date text-center p-2">
 								<span class="day">27</span>
 								<span class="mos">January</span>
@@ -711,8 +646,7 @@
 							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
 								there live the blind texts.</p>
 							<div class="d-flex align-items-center mt-4">
-								<p class="mb-0"><a href="#" class="btn btn-secondary">Read More <span
-											class="ion-ios-arrow-round-forward"></span></a></p>
+								<p class="mb-0"><a href="#" class="btn btn-secondary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
 								<p class="ml-auto mb-0">
 									<a href="#" class="mr-2">Admin</a>
 									<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
@@ -723,8 +657,7 @@
 				</div>
 				<div class="col-md-6 col-lg-4 ftco-animate">
 					<div class="blog-entry">
-						<a href="blog-single.html" class="block-20 d-flex align-items-end"
-							style="background-image: url('images/image_3.jpg');">
+						<a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/image_3.jpg');">
 							<div class="meta-date text-center p-2">
 								<span class="day">27</span>
 								<span class="mos">January</span>
@@ -736,8 +669,7 @@
 							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
 								there live the blind texts.</p>
 							<div class="d-flex align-items-center mt-4">
-								<p class="mb-0"><a href="#" class="btn btn-secondary">Read More <span
-											class="ion-ios-arrow-round-forward"></span></a></p>
+								<p class="mb-0"><a href="#" class="btn btn-secondary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
 								<p class="ml-auto mb-0">
 									<a href="#" class="mr-2">Admin</a>
 									<a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
@@ -754,32 +686,28 @@
 		<div class="container-wrap">
 			<div class="row no-gutters">
 				<div class="col-md-3 ftco-animate">
-					<a href="images/image_1.jpg" class="gallery image-popup img d-flex align-items-center"
-						style="background-image: url(images/course-1.jpg);">
+					<a href="images/image_1.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(images/course-1.jpg);">
 						<div class="icon mb-4 d-flex align-items-center justify-content-center">
 							<span class="icon-instagram"></span>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-3 ftco-animate">
-					<a href="images/image_2.jpg" class="gallery image-popup img d-flex align-items-center"
-						style="background-image: url(images/image_2.jpg);">
+					<a href="images/image_2.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(images/image_2.jpg);">
 						<div class="icon mb-4 d-flex align-items-center justify-content-center">
 							<span class="icon-instagram"></span>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-3 ftco-animate">
-					<a href="images/image_3.jpg" class="gallery image-popup img d-flex align-items-center"
-						style="background-image: url(images/image_3.jpg);">
+					<a href="images/image_3.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(images/image_3.jpg);">
 						<div class="icon mb-4 d-flex align-items-center justify-content-center">
 							<span class="icon-instagram"></span>
 						</div>
 					</a>
 				</div>
 				<div class="col-md-3 ftco-animate">
-					<a href="images/image_4.jpg" class="gallery image-popup img d-flex align-items-center"
-						style="background-image: url(images/image_4.jpg);">
+					<a href="images/image_4.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(images/image_4.jpg);">
 						<div class="icon mb-4 d-flex align-items-center justify-content-center">
 							<span class="icon-instagram"></span>
 						</div>
@@ -788,130 +716,3 @@
 			</div>
 		</div>
 	</section>
-
-
-	<footer class="ftco-footer ftco-bg-dark ftco-section">
-		<div class="container">
-			<div class="row mb-5">
-				<div class="col-md-6 col-lg-3">
-					<div class="ftco-footer-widget mb-5">
-						<h2 class="ftco-heading-2">Have a Questions?</h2>
-						<div class="block-23 mb-3">
-							<ul>
-								<li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain
-										View, San Francisco, California, USA</span></li>
-								<li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929
-											210</span></a></li>
-								<li><a href="#"><span class="icon icon-envelope"></span><span
-											class="text">info@yourdomain.com</span></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 col-lg-3">
-					<div class="ftco-footer-widget mb-5">
-						<h2 class="ftco-heading-2">Recent Blog</h2>
-						<div class="block-21 mb-4 d-flex">
-							<a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
-							<div class="text">
-								<h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a>
-								</h3>
-								<div class="meta">
-									<div><a href="#"><span class="icon-calendar"></span> Dec 25, 2018</a></div>
-									<div><a href="#"><span class="icon-person"></span> Admin</a></div>
-									<div><a href="#"><span class="icon-chat"></span> 19</a></div>
-								</div>
-							</div>
-						</div>
-						<div class="block-21 mb-5 d-flex">
-							<a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
-							<div class="text">
-								<h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a>
-								</h3>
-								<div class="meta">
-									<div><a href="#"><span class="icon-calendar"></span> Dec 25, 2018</a></div>
-									<div><a href="#"><span class="icon-person"></span> Admin</a></div>
-									<div><a href="#"><span class="icon-chat"></span> 19</a></div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 col-lg-3">
-					<div class="ftco-footer-widget mb-5 ml-md-4">
-						<h2 class="ftco-heading-2">Links</h2>
-						<ul class="list-unstyled">
-							<li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
-							<li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>About</a></li>
-							<li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Services</a></li>
-							<li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Deparments</a></li>
-							<li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-md-6 col-lg-3">
-					<div class="ftco-footer-widget mb-5">
-						<h2 class="ftco-heading-2">Subscribe Us!</h2>
-						<form action="#" class="subscribe-form">
-							<div class="form-group">
-								<input type="text" class="form-control mb-2 text-center"
-									placeholder="Enter email address">
-								<input type="submit" value="Subscribe" class="form-control submit px-3">
-							</div>
-						</form>
-					</div>
-					<div class="ftco-footer-widget mb-5">
-						<h2 class="ftco-heading-2 mb-0">Connect With Us</h2>
-						<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
-							<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-							<li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-							<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12 text-center">
-
-					<p>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;<script>
-							document.write(new Date().getFullYear());
-						</script> All rights reserved | This template is made with <i class="icon-heart"
-							aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-					</p>
-				</div>
-			</div>
-		</div>
-	</footer>
-
-
-
-	<!-- loader -->
-	<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
-			<circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-			<circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
-				stroke="#F96D00" /></svg></div>
-
-
-	<script src="js/jquery.min.js"></script>
-	<script src="js/jquery-migrate-3.0.1.min.js"></script>
-	<script src="js/popper.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.easing.1.3.js"></script>
-	<script src="js/jquery.waypoints.min.js"></script>
-	<script src="js/jquery.stellar.min.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<script src="js/aos.js"></script>
-	<script src="js/jquery.animateNumber.min.js"></script>
-	<script src="js/scrollax.min.js"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
-	</script>
-	<script src="js/google-map.js"></script>
-	<script src="js/main.js"></script>
-
-</body>
-
-</html>
