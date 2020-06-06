326fce1c5a85dff466113f9619583d5ff6aca339
Muhammad Frandika <muhammadfrade@gmail.com>
2020-06-06 20:45:34 +0700

    penambahan view tentang/harga/kontak/kursus

---
+++ application/views/harga.php
@@ -0,0 +1,86 @@
+
+      <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');">
+          <div class="overlay"></div>
+          <div class="container">
+              <div class="row no-gutters slider-text align-items-center justify-content-center">
+                  <div class="col-md-9 ftco-animate text-center">
+                      <h1 class="mb-2 bread">Daftar Harga</h1>
+                      <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
+                                      class="ion-ios-arrow-forward"></i></a></span> <span>Daftar Harga <i
+                                  class="ion-ios-arrow-forward"></i></span></p>
+                  </div>
+              </div>
+          </div>
+      </section>
+
+    <section class="ftco-section" style="padding: 4em 0 !important">
+        <div class="container">
+           
+            <div class="row">
+                <div class="col-md-6 col-lg-3 ftco-animate">
+                    <div class="pricing-entry bg-light pb-4 text-center">
+                        <div>
+                            <h3 class="mb-3">Basic</h3>
+                            <p><span class="price">$24.50</span> <span class="per">/ 5mos</span></p>
+                        </div>
+                        <div class="img" style="background-image: url(images/bg_1.jpg);">
+                        </div>
+                        <div class="px-4">
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                                there live the blind texts.</p>
+                        </div>
+                        <p class="button text-center"><a href="#" class="btn btn-primary px-4 py-3">Take A Course</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 ftco-animate">
+                    <div class="pricing-entry bg-light pb-4 text-center">
+                        <div>
+                            <h3 class="mb-3">Standard</h3>
+                            <p><span class="price">$34.50</span> <span class="per">/ 5mos</span></p>
+                        </div>
+                        <div class="img" style="background-image: url(images/bg_2.jpg);">
+                        </div>
+                        <div class="px-4">
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                                there live the blind texts.</p>
+                        </div>
+                        <p class="button text-center"><a href="#" class="btn btn-secondary px-4 py-3">Take A Course</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 ftco-animate">
+                    <div class="pricing-entry bg-light active pb-4 text-center">
+                        <div>
+                            <h3 class="mb-3">Premium</h3>
+                            <p><span class="price">$54.50</span> <span class="per">/ 5mos</span></p>
+                        </div>
+                        <div class="img" style="background-image: url(images/bg_3.jpg);">
+                        </div>
+                        <div class="px-4">
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                                there live the blind texts.</p>
+                        </div>
+                        <p class="button text-center"><a href="#" class="btn btn-tertiary px-4 py-3">Take A Course</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 ftco-animate">
+                    <div class="pricing-entry bg-light pb-4 text-center">
+                        <div>
+                            <h3 class="mb-3">Platinum</h3>
+                            <p><span class="price">$89.50</span> <span class="per">/ 5mos</span></p>
+                        </div>
+                        <div class="img" style="background-image: url(images/bg_5.jpg);">
+                        </div>
+                        <div class="px-4">
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                                there live the blind texts.</p>
+                        </div>
+                        <p class="button text-center"><a href="#" class="btn btn-quarternary px-4 py-3">Take A
+                                Course</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
---
+++ application/views/tentang.php
@@ -0,0 +1,153 @@
+
+    
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Tentang Kami</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="#">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Tentang<i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+		
+		<section class="ftco-section ftco-no-pt ftc-no-pb">
+		    <div class="container">
+		        <div class="row">
+		            <div class="col-md-5 order-md-last wrap-about py-5 wrap-about bg-light">
+		                <div class="text px-4 ftco-animate">
+		                    <h2 class="mb-4">Selamat datang di B'Smart Palembang</h2>
+		                    <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it
+		                        would have been rewritten a thousand times and everything that was left from its origin
+		                        would be the word.</p>
+		                    <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
+		                        ocean. A small river named Duden flows by their place and supplies it with the necessary
+		                        regelialia. And if she hasn’t been rewritten, then they are still using her.</p>
+		                    <p><a href="#" class="btn btn-secondary px-4 py-3">Read More</a></p>
+		                </div>
+		            </div>
+		            <div class="col-md-7 wrap-about py-5 pr-md-4 ftco-animate">
+		                <h2 class="mb-4">Apa yang kita tawarkan</h2>
+		                <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it
+		                    would have been rewritten a thousand times and everything that was left from its origin would be
+		                    the word.</p>
+		                <div class="row mt-5">
+		                    <div class="col-lg-6">
+		                        <div class="services-2 d-flex">
+		                            <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
+		                                    class="flaticon-security"></span></div>
+		                            <div class="text">
+		                                <h3>Keselamatan</h3>
+		                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+		                            </div>
+		                        </div>
+		                    </div>
+		                    <div class="col-lg-6">
+		                        <div class="services-2 d-flex">
+		                            <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
+		                                    class="flaticon-reading"></span></div>
+		                            <div class="text">
+		                                <h3>Kelas Reguler</h3>
+		                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+		                            </div>
+		                        </div>
+		                    </div>
+		                    <div class="col-lg-6">
+		                        <div class="services-2 d-flex">
+		                            <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
+		                                    class="flaticon-diploma"></span></div>
+		                            <div class="text">
+		                                <h3>Guru Bersertifikat</h3>
+		                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+		                            </div>
+		                        </div>
+		                    </div>
+		                    <div class="col-lg-6">
+		                        <div class="services-2 d-flex">
+		                            <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
+		                                    class="flaticon-education"></span></div>
+		                            <div class="text">
+		                                <h3>Ruang Kelas yang Cukup</h3>
+		                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+		                            </div>
+		                        </div>
+		                    </div>
+		                    <div class="col-lg-6">
+		                        <div class="services-2 d-flex">
+		                            <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
+		                                    class="flaticon-jigsaw"></span></div>
+		                            <div class="text">
+		                                <h3>Pelajaran Kreatif</h3>
+		                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+		                            </div>
+		                        </div>
+		                    </div>
+		                    <div class="col-lg-6">
+		                        <div class="services-2 d-flex">
+		                            <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span
+		                                    class="flaticon-kids"></span></div>
+		                            <div class="text">
+		                                <h3>Fasilitas olahraga</h3>
+		                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+		                            </div>
+		                        </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		</section>
+
+		<section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(images/bg_4.jpg);" data-stellar-background-ratio="0.5">
+    	<div class="container">
+    		<div class="row justify-content-center mb-5 pb-2">
+          <div class="col-md-8 text-center heading-section heading-section-black ftco-animate">
+            <h2 class="mb-4"><span>20 Years of</span> Experience</h2>
+            <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+          </div>
+        </div>	
+    		<div class="row d-md-flex align-items-center justify-content-center">
+    			<div class="col-lg-10">
+    				<div class="row d-md-flex align-items-center">
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="18">0</strong>
+		                <span>Certified Teachers</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="351">0</strong>
+		                <span>Successful Kids</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="564">0</strong>
+		                <span>Happy Parents</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="300">0</strong>
+		                <span>Awards Won</span>
+		              </div>
+		            </div>
+		          </div>
+	          </div>
+          </div>
+        </div>
+    	</div>
+    </section>
