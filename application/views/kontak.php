8d6eddc59c468776f832181a09cf7b1325c7d088
Muhammad Frandika <muhammadfrade@gmail.com>
2020-06-06 20:50:28 +0700

    Perubahan view tentang/kontak/harga/kursus

--- application/views/kontak.php
+++ application/views/kontak.php
@@ -1,44 +1,42 @@
- 
-   <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center">
-          <div class="col-md-9 ftco-animate text-center">
-            <h1 class="mb-2 bread">Kontak Kami</h1>
-            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Kontak <i class="ion-ios-arrow-forward"></i></span></p>
-          </div>
-        </div>
-      </div>
-    </section>
+ <section class="hero-wrap hero-wrap-2" style="background-image: url('<?= base_url(); ?>assets/User/images/bg_2.jpg');">
+   <div class="overlay"></div>
+   <div class="container">
+     <div class="row no-gutters slider-text align-items-center justify-content-center">
+       <div class="col-md-9 ftco-animate text-center">
+         <h1 class="mb-2 bread">Kontak Kami</h1>
+         <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Kontak <i class="ion-ios-arrow-forward"></i></span></p>
+       </div>
+     </div>
+   </div>
+ </section>
 
-    <section class="ftco-section contact-section">
-      <div class="container">
-        <div class="row d-flex mb-5 contact-info">
-          <div class="col-md-3 d-flex">
-          	<div class="bg-light align-self-stretch box p-4 text-center">
-          		<h3 class="mb-4">Address</h3>
-	            <p>198 West 21th Street, Suite 721 New York NY 10016</p>
-	          </div>
-          </div>
-          <div class="col-md-3 d-flex">
-          	<div class="bg-light align-self-stretch box p-4 text-center">
-          		<h3 class="mb-4">Contact Number</h3>
-	            <p><a href="tel://1234567920">+ 1235 2355 98</a></p>
-	          </div>
-          </div>
-          <div class="col-md-3 d-flex">
-          	<div class="bg-light align-self-stretch box p-4 text-center">
-          		<h3 class="mb-4">Email Address</h3>
-	            <p><a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-	          </div>
-          </div>
-          <div class="col-md-3 d-flex">
-          	<div class="bg-light align-self-stretch box p-4 text-center">
-          		<h3 class="mb-4">Website</h3>
-	            <p><a href="#">yoursite.com</a></p>
-	          </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
+ <section class="ftco-section contact-section">
+   <div class="container">
+     <div class="row d-flex mb-5 contact-info">
+       <div class="col-md-3 d-flex">
+         <div class="bg-light align-self-stretch box p-4 text-center">
+           <h3 class="mb-4">Address</h3>
+           <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+         </div>
+       </div>
+       <div class="col-md-3 d-flex">
+         <div class="bg-light align-self-stretch box p-4 text-center">
+           <h3 class="mb-4">Contact Number</h3>
+           <p><a href="tel://1234567920">+ 1235 2355 98</a></p>
+         </div>
+       </div>
+       <div class="col-md-3 d-flex">
+         <div class="bg-light align-self-stretch box p-4 text-center">
+           <h3 class="mb-4">Email Address</h3>
+           <p><a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+         </div>
+       </div>
+       <div class="col-md-3 d-flex">
+         <div class="bg-light align-self-stretch box p-4 text-center">
+           <h3 class="mb-4">Website</h3>
+           <p><a href="#">yoursite.com</a></p>
+         </div>
+       </div>
+     </div>
+   </div>
+ </section>
--- application/views/kursus.php
+++ application/views/kursus.php
@@ -1,48 +1,45 @@
+<section class="hero-wrap hero-wrap-2" style="background-image: url('<?= base_url(); ?>assets/User/images/bg_2.jpg');">
+	<div class="overlay"></div>
+	<div class="container">
+		<div class="row no-gutters slider-text align-items-center justify-content-center">
+			<div class="col-md-9 ftco-animate text-center">
+				<h1 class="mb-2 bread">Kursus Kami</h1>
+				<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Kursus <i class="ion-ios-arrow-forward"></i></span></p>
+			</div>
+		</div>
+	</div>
+</section>
 
-    
-    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center">
-          <div class="col-md-9 ftco-animate text-center">
-            <h1 class="mb-2 bread">Kursus Kami</h1>
-            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Kursus <i class="ion-ios-arrow-forward"></i></span></p>
-          </div>
-        </div>
-      </div>
-    </section>
-		
-		<section style="padding: 3em 0 !important">
-		    <div class="container">
-		        <div class="row justify-content-center mb-5 pb-2">
-		            <div class="col-md-8 text-center heading-section ftco-animate">
-		                <h2 class="mb-4"><span>Kursus</span> Kami</h2>
-		                <p>Separated they live in. A small river named Duden flows by their place and supplies it with the
-		                    necessary regelialia. It is a paradisematic country</p>
-		            </div>
-		        </div>
-		        <div class="row">
-		            <div class="col-md-6 course d-lg-flex ftco-animate">
-		                <div class="img" style="background-image: url(images/course-3.jpg);">
-		                </div>
-		                <div class="text bg-light p-4">
-		                    <h3><a href="#">Cinta Baca</a></h3>
-		                    <p class="subheading"><span>Class time:</span> 9:00am - 10am</p>
-		                    <p>Separated they live in. A small river named Duden flows by their place and supplies it with
-		                        the necessary regelialia. It is a paradisematic country</p>
-		                </div>
-		            </div>
-		            <div class="col-md-6 course d-lg-flex ftco-animate">
-		                <div class="img" style="background-image: url(images/course-4.jpg);">
-		                </div>
-		                <div class="text bg-light p-4">
-		                    <h3><a href="#">Cinta Matematika</a></h3>
-		                    <p class="subheading"><span>Class time:</span> 9:00am - 10am</p>
-		                    <p>Separated they live in. A small river named Duden flows by their place and supplies it with
-		                        the necessary regelialia. It is a paradisematic country</p>
-		                </div>
-		            </div>
-		        </div>
-		    </div>
-		</section>
-
+<section style="padding: 3em 0 !important">
+	<div class="container">
+		<div class="row justify-content-center mb-5 pb-2">
+			<div class="col-md-8 text-center heading-section ftco-animate">
+				<h2 class="mb-4"><span>Kursus</span> Kami</h2>
+				<p>Separated they live in. A small river named Duden flows by their place and supplies it with the
+					necessary regelialia. It is a paradisematic country</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6 course d-lg-flex ftco-animate">
+				<div class="img" style="background-image: url(<?= base_url(); ?>assets/User/images/course-3.jpg);">
+				</div>
+				<div class="text bg-light p-4">
+					<h3><a href="#">Cinta Baca</a></h3>
+					<p class="subheading"><span>Class time:</span> 9:00am - 10am</p>
+					<p>Separated they live in. A small river named Duden flows by their place and supplies it with
+						the necessary regelialia. It is a paradisematic country</p>
+				</div>
+			</div>
+			<div class="col-md-6 course d-lg-flex ftco-animate">
+				<div class="img" style="background-image: url(<?= base_url(); ?>assets/User/images/course-4.jpg);">
+				</div>
+				<div class="text bg-light p-4">
+					<h3><a href="#">Cinta Matematika</a></h3>
+					<p class="subheading"><span>Class time:</span> 9:00am - 10am</p>
+					<p>Separated they live in. A small river named Duden flows by their place and supplies it with
+						the necessary regelialia. It is a paradisematic country</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
