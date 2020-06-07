61f248d5c8ff1f8a1f9c8ec44a0d79a33ce80f27
Muhammad Frandika <muhammadfrade@gmail.com>
2020-06-07 15:22:05 +0700

    nambah add kelas

---
+++ application/views/admin/kelas/add_kelas.php
@@ -0,0 +1,109 @@
+            <!-- Bootstrap Select Css -->
+            <link href="<?= base_url(); ?>assets/AdminBsb/plugins/bootstrap-select/css/bootstrap-select.css" rel="stylesheet" />
+
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="block-header">
+                        <h2>B'SMART RUANG KELAS</h2>
+                    </div>
+
+                    <!-- Input -->
+                    <div class="row clearfix">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="card">
+                                <div class="header">
+                                    <h2 style="font-weight: bold;color: #ad1455;font-size: 22px;">
+                                        <center>FORM RUANG KELAS</center>
+                                        <br>
+                                    </h2>
+                                </div>
+
+                                <div class="body">
+                                    <form action="#" method="post" enctype="multipart/form-data">
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                                                <h4 style="font-size: 17.1px;">Kode Kelas</h4>
+                                            </div>
+                                            <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" class="form-control" placeholder="Kode Kelas" name="kode_kelas" required autocomplete="off" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                                                <h4 style="font-size: 17.1px;">Jenis Kelas</h4>
+                                            </div>
+                                            <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <select style="font-size: 14px;" class="form-control show-tick" name="jenis_kelas" required autocomplete="off">
+                                                            <option value="" selected="" disabled="">-- Pilih Status User --</option>
+                                                            <option value="Cinta Matika Pagi">Cinta Matika Pagi</option>
+                                                            <option value="Cinta Matika Siang">Cinta Matika Siang</option>
+                                                            <option value="Cinta Baca Pagi">Cinta Baca Pagi</option>
+                                                            <option value="Cinta Baca Siang">Cinta Baca Siang</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                                                <h4 style="font-size: 17.1px;">Jadwal Kelas</h4>
+                                            </div>
+                                            <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" class="form-control" placeholder="Jadwal Kelas" name="jadawl_kelas" required autocomplete="off" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                                                <h4 style="font-size: 17.1px;">Judul Kelas</h4>
+                                            </div>
+                                            <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" class="form-control" placeholder="judul Kelas" name="judul_kelas" required autocomplete="off" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row clearfix">
+                                            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                                                <h4 style="font-size: 17.1px;">Deskripsi Kelas</h4>
+                                            </div>
+                                            <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <textarea id="ckeditor" class="form-control" name="deskripsi_kelas" required autocomplete="off" /></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row clearfix">
+                                            <button class="btn btn-primary pull-right" style="margin-right: 20px;font-size: 16px;height: 40px;width: 100px;">SIMPAN</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- #END# Input -->
+
+                </div>
+                </div>
+            </section>
+
+            <!-- Select Plugin Js -->
+            <script src="<?= base_url(); ?>assets/AdminBsb/plugins/bootstrap-select/js/bootstrap-select.js"></script>
--- application/views/admin/kelas/list_kelas.php
+++ application/views/admin/kelas/list_kelas.php
@@ -1,88 +1,83 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.28.4/dist/sweetalert2.all.min.js"></script>
-      <?php if ($this->session->flashdata('message')): ?>
-        <script type="text/javascript">
-          swal({
-            title: "BERHASIL !!!",
-            text: "<?php echo $this->session->flashdata('message'); ?>",
-            showConfirmButton: true,
-            type: 'success'
-          });
-        </script>
-      <?php endif; ?>
-      <?php if ($this->session->flashdata('abort')): ?>
-        <script type="text/javascript">
-          swal({
-            title: "ERROR !!!",
-            text: "<?php echo $this->session->flashdata('abort'); ?>",
-            showConfirmButton: true,
-            type: 'error'
-          });
-        </script>
-      <?php endif; ?>
+    <?php if ($this->session->flashdata('message')) : ?>
+      <script type="text/javascript">
+        swal({
+          title: "BERHASIL !!!",
+          text: "<?php echo $this->session->flashdata('message'); ?>",
+          showConfirmButton: true,
+          type: 'success'
+        });
+      </script>
+    <?php endif; ?>
+    <?php if ($this->session->flashdata('abort')) : ?>
+      <script type="text/javascript">
+        swal({
+          title: "ERROR !!!",
+          text: "<?php echo $this->session->flashdata('abort'); ?>",
+          showConfirmButton: true,
+          type: 'error'
+        });
+      </script>
+    <?php endif; ?>
     <section class="content">
-        <div class="container-fluid">
-            <div class="block-header">
-                <h2>Kelas B'Smart</h2>
-            </div>
-
-             <!-- Basic Examples -->
-            <div class="row clearfix">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="card">
-                        <div class="header">
-                            <h2 style="font-size: 22px;color:#ad1455;font-weight: bold;">
-                              <center>List Kelas</center>
-                            </h2> <br><br>
-                            <a href="#">
-	                            <button type="button" class="btn btn-info waves-effect">
-		                            <i class="material-icons">add</i>
-		                            <span>TAMBAH</span>
-	                        	</button>
-                        	</a>
-                        </div>
-
-                        <div class="body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
-                                    <thead>
-                                        <tr>
-                                            <th>No</th>
-                                            <th>A</th>
-                                            <th>B</th>
-                                            <th>C</th>
-                                            <th>D</th>
-                                            <th>ACTION</th>
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-                                 
-                                        <tr>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td style="text-align: center;vertical-align: middle;">
-                                                <a href="" data-toggle="tooltip" data-placement="top" title="Edit"><i style="color:#00b0e4;" class="material-icons">description</i></a>&nbsp;&nbsp;
-                                                <a href="" data-toggle="tooltip" data-placement="top" title="Delete" onclick="javasciprt: return confirm('Yakin Ingin Menghapus ?')"><i style="color:red;" class="material-icons">delete</i></a>
-                                            </td>
-                                        </tr>
-                                     
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- #END# Basic Examples -->
-            </div>
+      <div class="container-fluid">
+        <div class="block-header">
+          <h2>B'SMART RUANG KELAS</h2>
         </div>
+
+        <!-- Basic Examples -->
+        <div class="row clearfix">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="card">
+              <div class="header">
+                <h2 style="font-size: 22px;color:#ad1455;font-weight: bold;">
+                  <center>LIST RUANG KELAS</center>
+                </h2> <br><br>
+                <a href="http://localhost/bimbel-online/admin/kelas/add_kelas">
+                  <button type="button" class="btn btn-info waves-effect">
+                    <i class="material-icons">add</i>
+                    <span>TAMBAH</span>
+                  </button>
+                </a>
+              </div>
+
+              <div class="body">
+                <div class="table-responsive">
+                  <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
+                    <thead>
+                      <tr>
+                        <th>No</th>
+                        <th>A</th>
+                        <th>B</th>
+                        <th>C</th>
+                        <th>D</th>
+                        <th style="text-align: center;">ACTION</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+
+                      <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td style="text-align: center;vertical-align: middle;">
+                          <a href="" data-toggle="tooltip" data-placement="top" title="Edit"><i style="color:#00b0e4;" class="material-icons">description</i></a>&nbsp;&nbsp;
+                          <a href="" data-toggle="tooltip" data-placement="top" title="Delete" onclick="javasciprt: return confirm('Yakin Ingin Menghapus ?')"><i style="color:red;" class="material-icons">delete</i></a>
+                        </td>
+                      </tr>
+
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- #END# Basic Examples -->
+      </div>
+      </div>
     </section>
-
-
-
-
-
