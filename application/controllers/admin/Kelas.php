a75d422df568ba7130c1a4019a7feafb7822076b
Yoga Anugrah Pratama <yogaanugrahpsy@gmail.com>
2020-06-07 13:33:13 +0700

    maintance controller klelas

--- application/controllers/admin/Kelas.php
+++ application/controllers/admin/Kelas.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Auth extends CI_Controller
+class Kelas extends CI_Controller
 {
 
 	public function __construct()
@@ -63,7 +63,7 @@ class Auth extends CI_Controller
 
 				$data['title'] = 'Add Kelas';
 				$data['layout'] = 'admin/add_kelas';
-				$this->load->view('admin/layout', $data);
+				$this->load->view('admin/layout_admin', $data);
 			} else {
 				$data = array(
 					'kode_kelas' => $this->security->xss_clean(get_kode_jenis_kelas($this->input->post('jenis_kelas'))),
@@ -88,7 +88,90 @@ class Auth extends CI_Controller
 
 			$data['title'] = 'Add Kelas';
 			$data['layout'] = 'admin/add_kelas';
-			$this->load->view('layout', $data);
+			$this->load->view('admin/layout_admin', $data);
 		}
+	}
+
+	public function edit_kelas($id)
+	{
+		if ($this->input->post('edit_kelas')) {
+			$this->form_validation->set_rules(
+				'kode_kelas',
+				'Kode Kelas',
+				'trim|required|min_length[3]',
+				array(
+					'required'    => '%s harus diisi!',
+					'min_length'  => '%s minimal 3 karakter!',
+				)
+			);
+			$this->form_validation->set_rules(
+				'judul_kelas',
+				'Judul Kelas',
+				'trim|required|min_length[3]',
+				array(
+					'required'    => '%s harus diisi!',
+					'min_length'  => '%s minimal 3 karakter!',
+				)
+			);
+			$this->form_validation->set_rules(
+				'deskripsi_kelas',
+				'Deskripsi Kelas',
+				'trim|required|min_length[3]',
+				array(
+					'required'    => '%s harus diisi!',
+					'min_length'  => '%s minimal 3 karakter!' //edited by wahid
+				)
+			);
+			$this->form_validation->set_rules(
+				'jenis_kelas',
+				'Jenis Kelas',
+				'trim|required',
+				array(
+					'required'    => '%s harus diisi!' //edited by wahid
+				)
+			);
+
+			$this->form_validation->set_rules('jadwal_kelas', 'Jadwal Kelas', 'trim|required');
+
+
+			if ($this->form_validation->run() == FALSE) {
+
+
+				$data['title'] = 'Edit Kelas';
+				$data['layout'] = 'admin/edit_kelas';
+				$this->load->view('admin/layout_admin', $data);
+			} else {
+				$data = array(
+					'kode_kelas' => $this->security->xss_clean(get_kode_jenis_kelas($this->input->post('jenis_kelas'))),
+					'jenis_kelas' => $this->security->xss_clean($this->input->post('jenis_kelas')),
+					'jadwal_kelas' => $this->security->xss_clean($this->input->post('jadwal_kelas')),
+					'judul_kelas' => $this->security->xss_clean($this->input->post('judul_kelas')),
+					'deskripsi_kelas' => $this->security->xss_clean($this->input->post('deskripsi_kelas')),
+					'created_at' => date('Y-m-d : h:m:s'),
+
+				);
+
+				$result = $this->kelas_model->update_kelas($data, $id);
+				if ($result) {
+					$this->session->set_flashdata('post_job_success', 'Congratulation! Job has been Posted successfully');
+					redirect(base_url('admin/kelas/list_kelas'));
+				} else {
+					$this->session->set_flashdata('post_job_error', 'Failed');
+					redirect(base_url('admin/kelas/add_kelas'));
+				}
+			}
+		} else {
+
+			$data['title'] = 'Add Kelas';
+			$data['layout'] = 'admin/edit_kelas';
+			$this->load->view('admin/layout_admin', $data);
+		}
+	}
+
+	public function delete_kelas($id)
+	{
+		$this->kelas_model->delete_kelas($id);
+		$this->session->set_flashdata('post_job_success', 'Congratulation! Job has been Posted successfully');
+		redirect(base_url('admin/kelas/list_kelas'));
 	}
 }
--- application/models/admin/Kelas_model.php
+++ application/models/admin/Kelas_model.php
@@ -10,4 +10,12 @@ class Kelas_model extends CI_Model
 		$this->db->insert('xx_kelas', $data);
 		return true;
 	}
+
+	// login function
+	public function update_kelas($data, $id)
+	{
+		$this->db->where($id, 'xx_kelas.id_kelas');
+		$this->db->update('xx_kelas', $data);
+		return true;
+	}
 }
