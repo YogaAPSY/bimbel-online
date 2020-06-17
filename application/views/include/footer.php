b00dc5c339ff10f85a21fe6e3cd75099cc258005
Yoga Anugrah Pratama <yogaanugrahpsy@gmail.com>
2020-06-17 10:58:28 +0700

    fix insyaAllah nian 2

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -8,7 +8,7 @@
 					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 					Copyright &copy;<script>
 						document.write(new Date().getFullYear());
-					</script> All rights reserved | Created <i class="icon-heart" aria-hidden="true"></i> by Geraldine
+					</script> All rights reserved | Created <i class="icon-heart" aria-hidden="true"></i> by Giral
 					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 				</p>
 			</div>
